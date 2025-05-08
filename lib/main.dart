5ae3d14f145e77b587da2fcd8472e0221d5b6b13
MoonKami81 <softwaretech81.pk@gmail.com>
2025-05-08 23:37:30 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'package:stadium_food/src/app.dart';
+import 'package:stadium_food/src/bloc/chat/chat_bloc.dart';
+import 'package:stadium_food/src/bloc/food/food_bloc.dart';
+import 'package:stadium_food/src/bloc/forgot_password/forgot_password_bloc.dart';
+import 'package:stadium_food/src/bloc/login/login_bloc.dart';
+import 'package:stadium_food/src/bloc/order/order_bloc.dart';
+import 'package:stadium_food/src/bloc/profile/profile_bloc.dart';
+import 'package:stadium_food/src/bloc/register/register_bloc.dart';
+import 'package:stadium_food/src/bloc/restaurant/restaurant_bloc.dart';
+import 'package:stadium_food/src/bloc/settings/settings_bloc.dart';
+import 'package:stadium_food/src/bloc/testimonial/testimonial_bloc.dart';
+import 'package:stadium_food/src/bloc/theme/theme_bloc.dart';
+import 'package:stadium_food/src/data/repositories/order_repository.dart';
+import 'package:stadium_food/src/data/services/hive_adapters.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Hive.initFlutter();
+  Hive.registerAdapter(FirestoreDocumentReferenceAdapter());
+  Hive.registerAdapter(RestaurantAdapter());
+  Hive.registerAdapter(FoodAdapter());
+  await Hive.openBox('myBox');
+
+  OrderRepository.loadCart();
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => RegisterBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LoginBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ForgotPasswordBloc(),
+        ),
+        BlocProvider(
+          create: (context) => RestaurantBloc(),
+        ),
+        BlocProvider(
+          create: (context) => FoodBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ProfileBloc(),
+        ),
+        BlocProvider(
+          create: (context) => OrderBloc(),
+        ),
+        BlocProvider(
+          create: (context) => TestimonialBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ChatBloc(),
+        ),
+        BlocProvider(
+          create: (context) => SettingsBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ThemeBloc(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}

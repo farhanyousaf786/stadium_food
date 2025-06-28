c0a17649a3f318354039308850fc04ac97fd1d98
Farhan Yousaf <58497319+farhanyousaf786@users.noreply.github.com>
2025-06-28 18:18:29 -0400

    added info screen in group chats, most of that data is static

--- lib/src/bloc/shop/shop_state.dart
+++ lib/src/bloc/shop/shop_state.dart
@@ -9,12 +9,10 @@ class ShopsLoading extends ShopState {}
 
 class ShopsLoaded extends ShopState {
   final List<Shop> shops;
-
   ShopsLoaded(this.shops);
 }
 
 class ShopError extends ShopState {
   final String message;
-
   ShopError(this.message);
 }
